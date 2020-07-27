bb2547cb33e2b7e88c52d48d2f9b23bac26e12ea
Luciano Olivera <oliveralucianof@gmail.com>
2020-07-27 11:37:54 -0300

    commit 3

---
+++ 003-paquetes/gato/hola.go
@@ -0,0 +1,13 @@
+package gato
+
+import "fmt"
+
+func Hola() {
+
+	fmt.Printf("hola desde gato")
+}
+
+func comen() {
+	fmt.Printf("comen pajaritos")
+
+}
---
+++ 003-paquetes/perro/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("hola desde perro")
+	gato.hola()
+	gato.Coman()
+
+}
